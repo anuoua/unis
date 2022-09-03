36ef0c7cfcf4fc4342e80b96d17350167a4f6b52
anuoua <anuoua@gmail.com>
2022-09-03 13:42:07 +0800

    fix(router): fix Link click event

--- packages/unis-router/src/components/Link.tsx
+++ packages/unis-router/src/components/Link.tsx
@@ -16,7 +16,7 @@ export type LinkProps = Partial<
 >;
 
 export const Link = (p: LinkProps) => {
-  let { to, children, ...rest } = useProps(p);
+  let { to, children, onClick, ...rest } = useProps(p);
 
   let { history } = useContext(RouterContext);
   let { route } = useContext(RouteContext);
@@ -24,8 +24,20 @@ export const Link = (p: LinkProps) => {
   let targetPath = use(() => resolvePath(route.pathname ?? "", to ?? ""));
 
   const handleJump = (e: MouseEvent) => {
-    history.location.pathname !== targetPath && history.push(targetPath);
-    e.preventDefault();
+    const target = (e.target as HTMLAnchorElement).target;
+    if (
+      e.button === 0 &&
+      (!target || target === "_self") &&
+      !(e.metaKey || e.altKey || e.ctrlKey || e.shiftKey)
+    ) {
+      if (history.location.pathname !== targetPath) {
+        history.replace(targetPath);
+      } else {
+        history.push(targetPath);
+      }
+      e.preventDefault();
+    }
+    onClick?.(e);
   };
 
   return () => (
