fad56b7a11afdce04581d804b4ac358c2ef20507
anuoua <anuoua@gmail.com>
2023-02-19 09:33:09 +0800

    chore(transition): upgrade dependencies

---
+++ packages/unis-transition/rollup.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from "rollup";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import esbuild from "rollup-plugin-esbuild";
+import { reassign } from "rollup-plugin-reassign";
+import { unisFns } from "@unis/unis";
+
+/**
+ * @param {*} format
+ * @returns {import('rollup').RollupOptions}
+ */
+const configGen = (format) =>
+  defineConfig({
+    input: "src/index.ts",
+    external: ["@unis/unis"],
+    output: [
+      {
+        dir: "build",
+        entryFileNames: `index.${format === "esm" ? "mjs" : "js"}`,
+        format,
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      esbuild({
+        sourceMap: true,
+        minify: process.env.NODE_ENV === "development" ? false : true,
+        target: "esnext",
+        jsxFactory: "h",
+      }),
+      reassign({
+        include: ["**/*.(t|j)s?(x)"],
+        targetFns: {
+          "@unis/unis": unisFns,
+        },
+      }),
+    ],
+  });
+
+const config = [configGen("cjs"), configGen("esm")];
+
+export default config;
