eafa45b28cd6151d2031c6a09b7288afbd0fba24
anuoua <anuoua@gmail.com>
2023-04-06 22:06:33 +0800

    chore(dom): rollup config update

--- packages/unis-dom/rollup.config.mjs
+++ packages/unis-dom/rollup.config.mjs
@@ -1,64 +1,56 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import esbuild from "rollup-plugin-esbuild";
 import dts from "rollup-plugin-dts";
+import { defineConfig } from "rollup";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-/**
- * @param {*} format
- * @returns {import('rollup').RollupOptions}
- */
-const configGen = (format) => ({
-  input: "build/browser/index.js",
-  external: [/^@unis/],
-  output: [
-    {
-      dir: "dist",
-      entryFileNames: `browser.${format === "esm" ? "mjs" : "js"}`,
-      format,
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    nodeResolve(),
-    esbuild({
-      sourceMap: true,
-      target: "esnext",
-    }),
-  ],
-});
+const configGen = (format) =>
+  defineConfig({
+    input: "build/browser/index.js",
+    external: [/^@unis/],
+    output: [
+      {
+        dir: "dist",
+        entryFileNames: `browser.${format === "esm" ? "mjs" : "js"}`,
+        format,
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      esbuild({
+        sourceMap: true,
+        target: "esnext",
+      }),
+    ],
+  });
 
-/**
- * @param {*} format
- * @returns {import('rollup').RollupOptions}
- */
-const serverConfigGen = (format) => ({
-  input: "build/server/index.js",
-  external: [/^@unis/],
-  output: [
-    {
-      dir: "dist",
-      entryFileNames: `server.${format === "esm" ? "mjs" : "js"}`,
-      format,
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    nodeResolve(),
-    esbuild({
-      sourceMap: true,
-      target: "esnext",
-    }),
-  ],
-});
+const serverConfigGen = (format) =>
+  defineConfig({
+    input: "build/server/index.js",
+    external: [/^@unis/],
+    output: [
+      {
+        dir: "dist",
+        entryFileNames: `server.${format === "esm" ? "mjs" : "js"}`,
+        format,
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      esbuild({
+        sourceMap: true,
+        target: "esnext",
+      }),
+    ],
+  });
 
-/**
- * @param {*} format
- * @returns {import('rollup').RollupOptions}
- */
-const dtsRollup = (which) => ({
-  input: `build/${which}/index.d.ts`,
-  output: [{ file: `dist/${which}.d.ts`, format: "es" }],
-  plugins: [dts()],
-});
+const dtsRollup = (which) =>
+  defineConfig({
+    input: `build/${which}/index.d.ts`,
+    output: [{ file: `dist/${which}.d.ts`, format: "es" }],
+    plugins: [dts()],
+  });
 
 const config = [
   configGen("cjs"),
