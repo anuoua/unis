54011e551fcb651638516b45187211d970f4e39a
anuoua <anuoua@gmail.com>
2023-04-16 21:40:47 +0800

    chore(example): update dependencies;

--- packages/unis-example/tailwind.config.js
+++ packages/unis-example/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
