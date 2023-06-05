819e72af1b3ff1606680b676ccf7564f86a4f079
anuoua <anuoua@gmail.com>
2023-06-05 10:29:22 +0800

    fix(core): stateEffect change

--- packages/unis-core/src/api/use.ts
+++ packages/unis-core/src/api/use.ts
@@ -7,7 +7,10 @@ export function use<T extends (...args: any[]) => any>(
 ): ReturnType<T>;
 export function use<T extends (...args: any[]) => any>(fn: T, raFn?: Function) {
   const workingFiber = getWF();
-  const effect = () => raFn?.(fn(getWF()));
+  const effect = () => {
+    const result = fn(getWF());
+    return raFn?.(result);
+  };
   workingFiber.stateEffects?.push(effect) ??
     (workingFiber.stateEffects = [effect]);
   return fn(workingFiber) as ReturnType<T>;
